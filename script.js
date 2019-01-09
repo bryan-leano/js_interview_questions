75250589e22bd03fad67bd70729176b1171a99db
Bryan Leanoe <bryan.leano@gmail.com>
2019-01-08 19:53:26 -0700

    IIFE

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function tripleAdd1(num1, num2, num3) {
 tripleAdd(10)(20)(30); 
 // returns total of all 3 numbers added together
 tripleAdd1(10, 20, 30);
+
+
+////////////////////////////////////
+// IIFE
+
+// Stands for Immediately Invoked Function Expressions
+// It's a function that is executed right after it's created
+// Used to create a private scope separate from the global scope
+
+(function doubleNumber(num) {
+  return num * 2;
+})(10);
