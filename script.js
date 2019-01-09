34926093068507baa3ac53a185c73df12bce744e
Bryan Leanoe <bryan.leano@gmail.com>
2019-01-08 18:59:45 -0700

    triple add

---
+++ script.js
@@ -0,0 +1,19 @@
+////////////////////////////////////
+// Triple Add
+
+// Currying function
+function tripleAdd(num1) {
+  return function(num2) {
+    return function(num3) {
+      return num1 + num2 + num3;
+    }
+  }
+};
+
+function tripleAdd1(num1, num2, num3) {
+  return num1 + num2 + num3;
+};
+
+tripleAdd(10)(20)(30); 
+// returns total of all 3 numbers added together
+tripleAdd1(10, 20, 30);
