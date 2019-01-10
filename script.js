c0562dcbfd5aa0c74761913a6f028040dea83592
Bryan Leanoe <bryan.leano@gmail.com>
2019-01-09 19:37:43 -0700

    closures

--- script.js
+++ script.js
@@ -66,3 +66,34 @@ function createButtons() {
 }
 
 createButtons();
+
+
+////////////////////////////////////
+// Closures
+
+// What is a closure?
+// A closure is an inner function that has access to the scope of an enclosing function.
+// A closure will have access to variables in 3 separate scopes:
+// 1. Variables in its own scope
+// 2. Variables in the scope of the outer function
+// 3. Variables in the global scope
+
+const globalVariable = 'global var';
+
+function outterFunc(param1) {
+  const variable1 = 'var one';
+
+  function innerFunc(param2) {
+    const variable2 = 'var two';
+
+    console.log('globalVariable: ', globalVariable);
+    console.log('variable1: ', variable1);
+    console.log('variable2: ', variable2);
+    console.log('param1: ', param1);
+    console.log('param2: ', param2);
+  }
+
+  innerFunc('param two');
+}
+
+outterFunc('param one');
